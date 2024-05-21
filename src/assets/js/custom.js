f277aa6a195cebd347dbab67fa0e48e14f997e47
Josh <jdelvecchio96@gmail.com>
2024-05-21 16:52:37 -0400

    Experimenting

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const accordionButtons = document.querySelectorAll(".accordion button");
+  const tabs = document.querySelectorAll(".tab-link");
+  const contents = document.querySelectorAll(".tab-content");
 
   accordionButtons.forEach((button) => {
     button.addEventListener("click", function () {
@@ -19,6 +21,22 @@ document.addEventListener("DOMContentLoaded", function () {
         panel.style.maxHeight = `${panel.scrollHeight + 30}px`;
         panel.classList.add("expanded");
       }
+    });
+  });
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      const target = this.getAttribute("data-tab");
+
+      tabs.forEach((t) => t.classList.remove("active"));
+      this.classList.add("active");
+
+      contents.forEach((content) => {
+        content.classList.remove("active");
+        if (content.id === target) {
+          content.classList.add("active");
+        }
+      });
     });
   });
 });
